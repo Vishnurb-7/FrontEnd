106ad06403cda236f9128a0bf5ace12ca3c5c0bb
Vishnurb-7 <vishnurb4vishnu@gmail.com>
2023-05-18 15:57:40 +0530

    after some ui updation

--- src/pages/adminPages/AdminLogin.jsx
+++ src/pages/adminPages/AdminLogin.jsx
@@ -61,8 +61,8 @@ const AdminLogin = () => {
           <div className='md:col-span-2 lg:col-span-1 flex flex-col items-center justify-center'>
               <img src={logo} alt="logo" width={330} />
               <h1 className='font-Viaoda text-7xl mb-10'>Login</h1>
-              <input onChange={valueSetting} type="text" name='name' value={admin.name} placeholder='Email' required className='w-[90%] h-20 mt-10 text-3xl border-2 border-black rounded-3xl text-center'/>
-        <input onChange={valueSetting} type={passwordType} name='password' value={admin.password} placeholder='Password' required className='w-[90%] h-20 mt-10 text-3xl border-2 border-black rounded-3xl text-center' />
+              <input onChange={valueSetting} type="text" name='name' value={admin.name} placeholder='Email' required className="w-[90%] h-16 mt-6 text-2xl bg-gray-200 border-2 border-black rounded-full text-center"/>
+        <input onChange={valueSetting} type={passwordType} name='password' value={admin.password} placeholder='Password' required className="w-[90%] h-16 mt-6 text-2xl bg-gray-200 border-2 border-black rounded-full text-center" />
         <p className="relative w-full ">
           <i className="absolute right-10 bottom-6 bg-white z-10 pl-2" onClick={passwordTypeChange}>
             {passwordVisible ? (
